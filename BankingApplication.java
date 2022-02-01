270f591393703a8af6e4a86d698b2155e5aec7e9
loosinehhartoonian <loosineh_10@yahoo.com>
2022-01-31 17:50:37 -0800

    First commit

---
+++ BankingApplication.java
@@ -0,0 +1,9 @@
+import BankAccount.BankAccount;
+public class BankingApplication
+{
+    public static void main(String[] args)
+    {
+        BankAccount obj1 = new BankAccount("XYZ", "BA001");
+        obj1.showMenue();
+    }
+}
